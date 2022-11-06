fb9f168398d76a6c70a6e4c6ba9bdd8d18444481
bomingeem <bomingeem@gmail.com>
2022-11-06 19:54:20 +0900

    Initial commit

---
+++ src/ch01/sec09/Hello.java
@@ -0,0 +1,12 @@
+package ch01.sec09;
+
+/**
+ * @author bomingeem
+ */
+public class Hello {
+    //프로그램 실행 진입점(entry point)
+    public static void main(String[] args) {
+        //콘솔에 출력하는 실행문
+        System.out.println("Hello, Java");
+    }
+}
