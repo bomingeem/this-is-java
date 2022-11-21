4cead5a8861c013e3b82748a8123547ad6ed1abf
bomingeem <bomingeem@gmail.com>
2022-11-21 22:34:26 +0900

    feat(chapter17): 스트림 요소 처리

---
+++ src/ch17/sec09/MatchingExample.java
@@ -0,0 +1,21 @@
+package ch17.sec09;
+
+import java.util.Arrays;
+
+public class MatchingExample {
+    public static void main(String[] args) {
+        int[] intArray = {2, 4, 6};
+
+        boolean result = Arrays.stream(intArray)
+                .allMatch(a -> a % 2 == 0);
+        System.out.println("모두 2의 배수인가? " + result);
+
+        result = Arrays.stream(intArray)
+                .anyMatch(a -> a % 3 == 0);
+        System.out.println("하나라도 3의 배수가 있는가? " + result);
+
+        result = Arrays.stream(intArray)
+                .noneMatch(a -> a%3 == 0);
+        System.out.println("3의 배수가 없는가? " + result);
+    }
+}
